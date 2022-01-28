8b5bd7d7193e90c5de2f4c885f946128e4b4a62a
Maria <lompemaria@gmail.com>
2022-01-28 14:51:34 +0100

    Calculator classes

---
+++ class_basic.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 14:37:53 2022
+
+@author: marialompe
+"""
+
+class basic(object):
+    def adding(self, num1, num2): 
+        answer = num1 + num2
+        print(answer)
+    def subtraction(self, num1, num2):
+        answer = num1 - num2
+        print(answer)
+    def multiplication(self, num1, num2):
+        answer = num1*num2
+        print(answer)
+    def division(self, num1, num2): 
+        answer = num1/num2
+        print(answer)
+   
+
